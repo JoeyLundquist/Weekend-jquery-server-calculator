91055abfd128330aa470dad8033d51a653a05a6e
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-19 21:51:39 -0500

    Set up basic server to run

---
+++ server/server.js
@@ -0,0 +1,19 @@
+//Importing express
+const express = require('express');
+//Importing body-parser
+const bodyParser = require('body-parser');
+//Creating express app
+const app = express();
+//Setting up port to run on
+const PORT = 5000;
+//Setting up body-parser to read objects on server side
+app.use(bodyParser.urlencoded({extended:true}));
+//Setting files for client machines
+app.use(express.static('server/public'));
+
+
+
+//Setting up the listener for PORT 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on`, PORT)
+})
