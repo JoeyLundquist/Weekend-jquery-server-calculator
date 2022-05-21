37be863de408cd99158359a301ca31f91fa3741b
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-20 20:01:05 -0500

    getting th calculation function started

--- server/public/cleint.js
+++ server/public/cleint.js
@@ -2,14 +2,25 @@ $(readyNow)
 
 function readyNow() {
     $('#calculator-form-input').on('submit', onSubmit)
+    $('.math-operators').on('click', mathOperatorSelect)
+    gatherInputs();
 }
+
+let mathOp = '';
+
+function mathOperatorSelect() {
+    mathOp = $(this).text();
+    console.log(mathOp);
+}
+
 
 function onSubmit(evt) {
     evt.preventDefault();
 
     let data = {
         firstInputNumber: $('#first-input-number').val(),
-        secondInputNumber: $('#second-input-number').val()
+        secondInputNumber: $('#second-input-number').val(),
+        mathOperator: mathOp
     }
 
     $.ajax({
@@ -20,5 +31,27 @@ function onSubmit(evt) {
         console.log('Inputs Posted')
     }).catch(() => {
         console.log(`Not posted, error`)
+    });
+    gatherInputs();
+}
+
+function gatherInputs() {
+    $.ajax({
+        url:'/calculator-objects',
+        method: 'GET'
+    }).then((response) => {
+        console.log(`Did the GET`);
+        renderToDOM(response)
+
     })
+}
+
+function renderToDOM(calc) {
+    let el = $('#calculator-history')
+    el.empty();
+    for(let input of calc){
+    el.append(`
+        <li>${input.firstInputNumber} ${input.secondInputNumber}</li>
+    `)
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,21 +1,51 @@
 //Importing express
 const express = require('express');
+
 //Importing body-parser
 const bodyParser = require('body-parser');
+
 //Creating express app
 const app = express();
+
 //Setting up port to run on
 const PORT = 5000;
+
 //Setting up body-parser to read objects on server side
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
+
 //Setting files for client machines
 app.use(express.static('server/public'));
+
 //importing CalcObjects Array
 let calculatorObjects = require('./modules/calcObject');
 console.log(calculatorObjects);
 
+//Importing divide function
+const divide = require('./modules/divide')
+
+//Importing multiplication function
+const multiplication = require('./modules/multi')
+
+//Importing subtract function
+const subtract = require('./modules/subtract');
+
+//Importing addition function
+const sum = require('./modules/sum');
+
+function doingCalculations () {
+    for(let calcs of calculatorObjects){
+        if(calcs.mathOperator === '/'){
+             divide.divideNum(calcs.firstInputNumber, calcs.secondInputNumber);
+        }
+    }
+}
+
+
+
+
 app.get('/calculator-objects', (req, res) => {
+    console.log(`Inside calc GET`)
     res.send(calculatorObjects)
 })
 
